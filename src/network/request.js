97227590120df87b46eb70b6755064e39c24e94d
17671448657 <416317444@qq.com>
2020-10-16 07:55:34 +0800

    nothing

--- src/network/request.js
+++ src/network/request.js
@@ -9,27 +9,4 @@ export function request(config) {
   return instance(config)
 }
 
-  //拦截器
-
-  // //请求拦截
-  // instance.interceptors.request.use(config => {
-  //   console.log(config);
-  //   // 作用：
-  //   // 1.比如config中的一些信息不符合服务器的要求
-  //   // 2.比如每次发送网络请求时，希望在界面中显示一些图标
-  //   // 3.某些网络请求必须携带一些特殊的信息，比如登录的token
-  //   return config
-  // }, err => {
-  //   console.log(err);
-  // })
-
-  // //响应拦截
-  // instance.interceptors.response.use(res => {
-  //   console.log(res);
-  //   return res //注意操作完数据之后需要返回，否则拿不到数据
-  // }, err => {
-  //   console.log(err);
-  // })
-
-
 
